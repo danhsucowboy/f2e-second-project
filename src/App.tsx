dd47b518ac006913d5250fd125e31d517b56f7cb
Dan Hsu <hsu86110@gmail.com>
2021-08-04 16:45:06 +0800

    feature: main page layout done

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,107 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <div className="play-ground">
+          <div className="cell-wrapper">
+            <div className="open-cell">
+              <div className="cell"></div>
+              <div className="cell"></div>
+              <div className="cell"></div>
+              <div className="cell"></div>
+            </div>
+            <div className="icon-wrapper">
+              <div className="king-n-logo">
+                <div className="king">
+                  {/* <img src="king_happy.png" alt="king-head" /> */}
+                </div>
+                <p className="title">FREECELL</p>
+              </div>
+            </div>
+            <div className="foundation">
+              <div className="cell base-bg">
+                <div className="mask-spade"></div> 
+              </div>
+              <div className="cell base-bg">
+                <div className="mask-heart"></div>
+              </div>
+              <div className="cell base-bg">
+                <div className="mask-diamond"></div>
+              </div>
+              <div className="cell base-bg">
+                <div className="mask-club"></div>
+              </div>
+            </div>
+          </div>
+          <div className="card-table">
+            <div className="drag-section">
+              <div className="box">
+                <div className="card"></div>
+              </div>
+              <div className="box">
+                <div className="card"></div>
+              </div>
+              <div className="box">
+                <div className="card"></div>
+              </div>
+              <div className="box">
+                <div className="card"></div>
+              </div>
+              <div className="box">
+                <div className="card"></div>
+              </div>
+              <div className="box">
+                <div className="card"></div>
+              </div>
+              <div className="box">
+                <div className="card"></div>
+              </div>
+              <div className="box">
+                <div className="card"></div>
+              </div>
+              <div className="box">
+                <div className="card"></div>
+              </div>
+              <div className="box">
+                <div className="card"></div>
+              </div>
+              <div className="box">
+                <div className="card"></div>
+              </div>
+              <div className="box">
+                <div className="card"></div>
+              </div>
+              <div className="box">
+                <div className="card"></div>
+              </div>
+              <div className="box">
+                <div className="card"></div>
+              </div>
+              <div className="box">
+                <div className="card"></div>
+              </div>
+              <div className="box">
+                <div className="card"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="control-panel">
+          <div className="board">
+            <button className="show-rule">
+              <div className="spade"></div>
+            </button>
+            <div className="timer board-text">TIME: 00:00</div>
+            <div className="score board-text">SCORE: 0</div>
+          </div>
+          <div className="buttons">
+            <button className="main-control">NEW GAME</button>
+            <button className="main-control">RESTART</button>
+            <button className="main-control">HINT</button>
+            <button className="main-control">UNDO</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
